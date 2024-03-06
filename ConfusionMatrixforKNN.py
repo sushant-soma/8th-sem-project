95d980c867b5eda26bca1b9a6a7b17f2b6c22000
sushant-soma <ksoe.codes@mail.com>
2024-03-06 10:48:00 +0530

    KNN commit

--- ConfusionMatrixforKNN.py
+++ ConfusionMatrixforKNN.py
@@ -6,50 +6,40 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
 
-# import some data to play with
 data=pd.read_csv('T3resin1.txt')
 data=pd.DataFrame(data)
 
 X=data.iloc[:,2:4].values
-#X=pd.DataFrame(data.iloc[:,3:4])
+
 Y=data.iloc[:,:1].values
-#Y=pd.DataFrame(data.iloc[:,:1])
+
 class_names = np.arange(0,2,dtype=np.int)
 print(class_names)
 
-# Split the data into a training set and a test set
-#X_train, X_test, y_train, y_test, = train_test_split(X, y, random_state=1)
 number_of_samples = len(Y)
 
-#Splitting into training, validation and test sets
 random_indices = np.random.permutation(number_of_samples)
-#Training set
+
 num_training_samples = int(number_of_samples*0.7)
 x_train = X[random_indices[:num_training_samples]]
 y_train = Y[random_indices[:num_training_samples]]
-#Validation set
+
 num_validation_samples = int(number_of_samples*0.15)
 x_val = X[random_indices[num_training_samples : num_training_samples+num_validation_samples]]
 y_val = Y[random_indices[num_training_samples: num_training_samples+num_validation_samples]]
-#Test set
+
 num_test_samples = int(number_of_samples*0.15)
 x_test = X[random_indices[-num_test_samples:]]
 y_test = Y[random_indices[-num_test_samples:]]
 
-#Visualizing the training data
-X_class0 = np.asmatrix([x_train[i] for i in range(len(x_train)) if y_train[i]==0]) #Picking only the first two classes
+X_class0 = np.asmatrix([x_train[i] for i in range(len(x_train)) if y_train[i]==0]) 
 Y_class0 = np.zeros((X_class0.shape[0]),dtype=np.int)
 X_class1 = np.asmatrix([x_train[i] for i in range(len(x_train)) if y_train[i]==1])
 Y_class1 = np.ones((X_class1.shape[0]),dtype=np.int)
 
-
-# Run classifier, using a model that is too regularized (C too low) to see
-# the impact on the results
 classifier = KNeighborsClassifier(n_neighbors = 5) 
-#classifier = svm.SVC(kernel='linear', C=0.01)
+
 y_pred = classifier.fit(x_train, y_train).predict(x_test)
-
-
 
 def plot_confusion_matrix(y_true, y_pred, classes,
                           normalize=False,
@@ -65,9 +55,8 @@ def plot_confusion_matrix(y_true, y_pred, classes,
         else:
             title = 'Confusion matrix, without normalization'
 
-    # Compute confusion matrix
     cm = confusion_matrix(y_true, y_pred)
-    # Only use the labels that appear in the data
+
     classes = classes[unique_labels(y_true, y_pred)]
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -80,20 +69,18 @@ def plot_confusion_matrix(y_true, y_pred, classes,
     fig, ax = plt.subplots()
     im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
     ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
+
     ax.set(xticks=np.arange(cm.shape[1]),
            yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
+
            xticklabels=classes, yticklabels=classes,
            title=title,
            ylabel='True label',
            xlabel='Predicted label')
 
-    # Rotate the tick labels and set their alignment.
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
              rotation_mode="anchor")
 
-    # Loop over data dimensions and create text annotations.
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i in range(cm.shape[0]):
@@ -104,14 +91,11 @@ def plot_confusion_matrix(y_true, y_pred, classes,
     fig.tight_layout()
     return ax
 
-
 np.set_printoptions(precision=2)
 
-# Plot non-normalized confusion matrix
 plot_confusion_matrix(y_test, y_pred, classes=class_names,
                       title='Confusion matrix, without normalization')
 
-# Plot normalized confusion matrix
 plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True,
                       title='Normalized confusion matrix')
 
